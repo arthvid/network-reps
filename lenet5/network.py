1d739e8c1ef894405b0b23b30dbdc5da8be11b36
Arth Vidyarthi <avidyarthi@Arths-MacBook-Air.local>
2024-03-05 21:55:36 -0600

    add training loop

--- lenet5/network.py
+++ lenet5/network.py
@@ -9,13 +9,12 @@ class LeNet5(nn.Module):
        super().__init__()
        
        self.tanh = nn.Tanh()
-       self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=(5,5), stride=1)
+       self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(5, 5), stride=1)
        self.avgpool1 = nn.AvgPool2d(kernel_size=(2,2))
        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=(5,5), stride=1)
        self.avgpool2 = nn.AvgPool2d(kernel_size=(2,2))
-       self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=(5,5), stride=1)
-       self.fc1 = nn.Linear(in_features=120, out_features=84)
-       self.fc2 = nn.Linear(in_features=84, out_features=10)
+       self.fc1 = nn.Linear(in_features=256, out_features=64)
+       self.fc2 = nn.Linear(in_features=64, out_features=10)
        self.softmax = nn.Softmax()
     
     def forward(self, x):
@@ -25,9 +24,7 @@ class LeNet5(nn.Module):
         x = self.conv2(x)
         x = self.avgpool2(x)
         x = self.tanh(x)
-        x = self.conv3(x)
-        x = self.tanh(x)
-        x =  x.reshape(1, x.size(0))
+        x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.tanh(x)
         x = self.fc2(x)
--- lenet5/run.py
+++ lenet5/run.py
@@ -5,8 +5,11 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import pdb
 from network import LeNet5
+from tqdm import tqdm
 
 
+# Hyperparameters
+batch_size = 4
 # Prepare the dataset.
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -16,15 +19,39 @@ training_set = torchvision.datasets.MNIST(root='.', download=True, train=True, t
 validation_set = torchvision.datasets.MNIST(root='.', download=True, train=False, transform=transform)
 
 # Create the dataloader.
-train_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True)
-val_loader = torch.utils.data.DataLoader(validation_set, batch_size=4, shuffle=False)
+train_loader = torch.utils.data.DataLoader(training_set, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(validation_set, batch_size=batch_size, shuffle=False)
 
 # Instantiate an instance of the network.
 model = LeNet5()
 
 # Select a loss function.
-loss = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters())
 
 
+number_epochs = 10
 
+for epoch in tqdm(range(number_epochs)):
+    running_loss = 0.
+    for idx, batch in enumerate(train_loader):
+        # Split element of dataloader into data points and labels.
+        inputs, labels = batch
+        # Zero any accumulated gradients inside optimizer.
+        optimizer.zero_grad()
+        # Execute forward pass.
+        pred = model(inputs)
+        # Calculate loss.
+        loss = loss_fn(pred, labels)
+        # Calculate gradients in backward pass.
+        loss.backward()
+        # Update gradients.
+        optimizer.step()
+        
+        # Gather data and report
+        running_loss += loss.item()
+    
+        
+print()
+        
+    
